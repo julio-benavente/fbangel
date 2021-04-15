4f4e3543b11af79d441a204d64b25de854dde566
Julio Benavente <32945829+julio-benavente@users.noreply.github.com>
2021-04-15 17:10:18 -0500

    Fixing bug. Website doesn't visualize in gh-pages

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
